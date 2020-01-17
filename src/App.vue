48c127759e7b4815243a8bb0ceb2e21c74e7b7dc
David Oliveira <dol.jpo@hotmail.com>
2020-01-17 20:27:05 -0300

    change background images, add scripts

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
 <template>
-  <div id="app">
+  <div id="app" :class="typeof weather.main != 'undefined' && weather.main.temp > 16 ? 'warm' : ''">
     <main>
       <div class="search-box">
-        <input type="text" class="search-bar" placeholder="Search..."/>
+        <input type="text" class="search-bar" placeholder="Search..." 
+        v-model="query" @keyup.enter="fetchWeather"/>
       </div>
 
-      <div class="weather-wrap">
+      <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
         <div class="location-box">
-          <div class="location">Joinville, SC</div>
-          <div class="date">Sexta, 17 de Janeiro, 2020</div>
+          <div class="location">{{ weather.name }}, {{ weather.sys.country }}</div>
+          <div class="date">{{ dateBuilder() }}</div>
         </div>
 
         <div class="weather-box">
-          <div class="temp">16º</div>
-          <div class="weather">Rain</div>
+          <div class="temp">{{ Math.round(weather.main.temp) + 'ºc' }}</div>
+          <div class="weather">{{ weather.weather[0].main }}</div>
         </div>
       </div>
     </main>
@@ -25,7 +26,38 @@
     name: 'app',
     data () {
       return {
-        api_key: 'bc44a72c029bcfd5792ce1315fdb4048'
+        api_key: 'bc44a72c029bcfd5792ce1315fdb4048',
+        url_base: 'https://api.openweathermap.org/data/2.5/',
+        query: '',
+        weather: {}
+      }
+    },
+
+    methods: {
+      fetchWeather () {
+        fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
+          .then(res => {
+            return res.json();
+          }).then(this.setResults);
+      },
+
+      setResults (results) {
+        this.weather = results;
+      },
+
+      dateBuilder () {
+        let d = new Date();
+        let months = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho',
+          'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro' ];
+        let days = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 
+          'Sexta', 'Sábado'];
+
+        let day = days[d.getDay()] + '-feira';
+        let date = d.getDate();
+        let month = months[d.getMonth()];
+        let year = d.getFullYear();
+
+        return `${day}, ${date} de ${month} de ${year}`;
       }
     }
   }
@@ -43,10 +75,14 @@
   }
 
   #app {
-    background-image: url('./assets/cold_3.jpg');
+    background-image: url('./assets/cold-bg.jpg');
     background-size: cover;
     background-position: bottom;
     transition: 0.4s;
+  }
+
+  #app.warm {
+    background-image: url('./assets/warm-bg.jpg');
   }
 
   main {
